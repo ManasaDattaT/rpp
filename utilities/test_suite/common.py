23888a28f8200ed850338d5a3f9504755abe1711
root <root@ctr-s95-mi300x-3.amd.com>
2025-12-22 13:47:26 +0000

    Extended cases for perf tests

--- utilities/test_suite/common.py
+++ utilities/test_suite/common.py
@@ -64,9 +64,9 @@ class BitDepthTestMode(Enum):
     U32_TO_U32 = 10 # Input: U8 -> Output: I8
     I8_TO_F32  = 11 # Input: I8  -> Output: F32
     I16_TO_F32 = 12 # Input: I16 -> Output: F32
-    U16_TO_F32 = 13, # Input: U16 -> Output: F32
-    U32_TO_F32 = 14, # Input: U32 -> Output: F32
-    I32_TO_F32 = 15  # Input: I32 -> Output: F32
+    U16_TO_F32 = 13 # Input: U16 -> Output: F32
+    U32_TO_F32 = 14 # Input: U32 -> Output: F32
+    I32_TO_F32 = 15 # Input: I32 -> Output: F32
 
 
 
